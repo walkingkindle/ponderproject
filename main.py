5a9fa5a2350bb1664fb7e21247b5cd6debf7e01d
Alex <aki.hadza@gmail.com>
2023-03-21 17:59:00 +0100

    Implementing Register.

--- main.py
+++ main.py
@@ -1,13 +1,63 @@
+#----------------------------------------------------------------------IMPORTS------------------------------------------
+
 import random
 import requests
 from bs4 import BeautifulSoup
 import itertools
+from flask import Flask, render_template, redirect, url_for, flash,request
+from flask_bootstrap import Bootstrap
+from flask_ckeditor import CKEditor
+from datetime import date
+from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.utils import secure_filename
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship
 from flask import Flask, render_template
+from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 from wtforms import StringField, SubmitField,SelectField
 from wtforms.validators import DataRequired,url
 import csv
+import uuid
+
+
+
+
+
+
+#-----------------------------------------------------------FLASK APP---------------------------------------------------
+#Initiating FLask APP
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['MAX_CONTENT_PATH'] = 1000000
+ALLOWED_EXTENSIONS = {'txt'}
+app.config['UPLOAD_FOLDER'] = r"C:\Users\Aleksa Hadzic\PycharmProjects\Ponderproject\documents"
+
+
+#Flask DB
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+users = SQLAlchemy(app)
+
+#Configure Tables
+class User(users.Model,UserMixin):
+        __tablename__ = "users"
+        id = users.Column(users.String(36), primary_key=True, default=str(uuid.uuid4()))
+        email = users.Column(users.String(250),nullable=False,unique=True)
+        username = users.Column(users.String(250),nullable=False,unique=True)
+        password = users.Column(users.String(1000),nullable=False,unique=True)
+
+
+# Creating Tables
+
+# with app.app_context():
+#     users.create_all()
+#     users.session.commit()
 
 #-----------------------------------------------------------ENGINE------------------------------------------------------
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 def get_random_quote_from_kindle():
     check_letter = '-'
     with open('documents/My Clippings.txt','r',encoding='utf-8') as quotes:
@@ -38,29 +88,73 @@ def also_get_random_quote():
         quote = f"{r['content']}, {r['author']}"
         print(quote)
         return quote
-get_random_quote()
-#-----------------------------------------------------------FLASK APP---------------------------------------------------
-app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 
 
+#-------------------------------------- FLASK ROUTES -----------------------------------------------------------------
 @app.route("/")
 def home():
     return render_template("Index.html",quote=get_random_quote())
 
 
+@app.route("/dashboard")
+def dashboard():
+    return render_template("Dashboard.html")
+
+
+@app.route('/choose-your-path')
+def choose_path():
+    return render_template("Upload File.html")
+
+
+# @app.route("/upload",methods=['GET','POST'])
+# def upload():
+#     if request.method['POST']:
+#         if 'My Clippings' not in request.files:
+#             flash("No file part")
+#             return redirect(url_for("upload"))
+#         file = request.files['My Clippings']
+#         if file.filename == ''
+#             flash("No selected file")
+#     return render_template("Kindle Upload.html")
+
+@app.route("/search-page")
+def search_page():
+    return render_template("Search.html")
+
+@app.route("/register",methods=['GET','POST'])
+def register():
+    if request.method == 'POST':
+        e_mail = request.form.get('e-mail')
+        username = request.form.get('username')
+        password = request.form.get('password')
+        hashed_password = generate_password_hash(password=password,method="pbkdf2:sha256",salt_length=8)
+        new_user = User(
+            email=e_mail,
+            password=hashed_password,
+            username=username
+        )
+        users.session.add(new_user)
+        users.session.commit()
+        return redirect(url_for("choose_path"))
+    return render_template("sign up.html")
 
 @app.route("/login")
 def log_in():
+    if request.method == 'POST':
+        entered_email = request.form.get('email')
+        entered_pasword = request.form.get('password')
+        user = users.session.query(User).filter_by(entered_email).first()
+        if user:
+            if check_password_hash(pwhash=user.password,password=entered_pasword):
+                login_user(user)
+                return redirect(url_for("dashboard"))
+            else:
+                flash("Invalid Password")
+                return redirect(url_for("login"))
+        else:
+            flash("This E-mail doesn't exist. Please create your account.")
+            return redirect(url_for("login"))
     return render_template("sign-in.html")
-
-
-
-@app.route("/register")
-def register():
-    return render_template("sign up.html")
-
-
 
 
 
