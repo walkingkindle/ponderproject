e6b4d6cc5618cce8a7693c85e5c5024e8fa80c58
walkingkindle <122030667+walkingkindle@users.noreply.github.com>
2023-06-09 11:09:36 +0200

    fixing atrribute not found error

--- app/run.py
+++ app/run.py
@@ -8,6 +8,7 @@ from flask_login import UserMixin, login_user, LoginManager, login_required, cur
 from flask_mail import Mail, Message
 from flask_bootstrap import Bootstrap
 from flask_ckeditor import CKEditor
+from flask_session import Session
 from sqlalchemy.exc import IntegrityError
 
 import engine
@@ -73,7 +74,9 @@ app.config['MAIL_PASSWORD'] = config.password
 app.config['SESSION_TYPE'] = 'filesystem'
 app.config['MAIL_SENDER'] = config.email
 app.config['MAIL_DEFAULT_ADDRESS'] = config.email
-
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 with app.app_context():
     users.create_all()
 
@@ -188,8 +191,9 @@ def notifications():
 
 @app.route('/choose-your-path')
 def choose_path():
-
-    return render_template("Upload-File.html", current_user=current_user,user_id=current_user.id)
+    user_id = session.get('id')
+    print(user_id)
+    return render_template("Upload-File.html", current_user=current_user,user_id=user_id)
 
 
 @app.route("/upload", methods=['GET', 'POST'])
@@ -360,6 +364,7 @@ def register():
             users.session.commit()
             token = s.dumps(e_mail,salt='email-confirm')
             link = url_for('confirm_email',token=token,_external=True,email=e_mail,user_id=new_user.id)
+            session['id'] = new_user.id
             msg = Message(' Confirm Email ', sender=app.config['MAIL_SENDER'], recipients=[e_mail],body=engine.confirmation_email(link))
             mail.send(msg)
             email_sent= True
