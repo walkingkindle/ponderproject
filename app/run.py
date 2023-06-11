da9dd8ae2dfa3cc77a8aa7f7e1297c5f19cb365b
walkingkindle <122030667+walkingkindle@users.noreply.github.com>
2023-06-11 15:07:57 +0200

    Fixing invalid csrf token deployment error

--- app/run.py
+++ app/run.py
@@ -64,8 +64,7 @@ from wikiquotes.managers.custom_exceptions import TitleNotFound
 # Initiating Flask APP
 app = initialize_app('sqlite:///users.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'If(CDsMkc%6xM4%$'
-app.config['CSRF_SECRET_KEY'] = 'If(CDsMkc%6xM4%$a'
+app.config['SECRET_KEY'] = config.urlsafe_secret
 app.config['MAX_CONTENT_PATH'] = 1000000
 app.config['MAIL_SERVER'] = "smtp.gmail.com"
 app.config['MAIL_PORT'] = 465
@@ -79,8 +78,6 @@ app.config['MAIL_DEFAULT_ADDRESS'] = config.email
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
-csrf = CSRFProtect()
-csrf.init_app(app)
 with app.app_context():
     users.create_all()
 
